4a37c314c5c49c834e51f3ecdaabd29f959071bc
Katsiaryna31 <katia310790@mail.ru>
2020-03-17 23:33:52 +0300

    feat: add version for deploy

---
+++ precache-manifest.e9be4977ccad9dcbc54413a77cda8be0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c7915f537666c9c7d25f753e12873f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5a57cb1d5a980b6eab",
+    "url": "/static/css/main.9498e5fe.chunk.css"
+  },
+  {
+    "revision": "279b33f34206b04f7dec",
+    "url": "/static/js/2.e13e54e3.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.e13e54e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5a57cb1d5a980b6eab",
+    "url": "/static/js/main.191e21e0.chunk.js"
+  },
+  {
+    "revision": "707d41e56b4e984fb3b1",
+    "url": "/static/js/runtime-main.335ad2b1.js"
+  },
+  {
+    "revision": "49ae26aafd8fa3aa029de4fcbce20824",
+    "url": "/static/media/Coronavirus.49ae26aa.svg"
+  },
+  {
+    "revision": "788e5bbe4fa5c10e1348dfdf5241bb42",
+    "url": "/static/media/Search.788e5bbe.svg"
+  },
+  {
+    "revision": "4fef2161ee6dcb90a8d19cc3eb870aae",
+    "url": "/static/media/Triangle.4fef2161.svg"
+  },
+  {
+    "revision": "daf70e72e94cc3ceb5da319811c206e8",
+    "url": "/static/media/TriangleActive.daf70e72.svg"
+  }
+]);
